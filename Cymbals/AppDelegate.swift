0dcbe83752c6bb86303d6b4b666cf9f76d6f7fb8
Peter Kraml <piets@macpiets.net>
2016-03-13 18:09:27 +0100

    Initial Commit

---
+++ Cymbals/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Cymbals
+//
+//  Created by Peter Kraml on 13.03.16.
+//  Copyright © 2016 MacPiets Apps. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Cymbals/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Cymbals
+//
+//  Created by Peter Kraml on 13.03.16.
+//  Copyright © 2016 MacPiets Apps. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
